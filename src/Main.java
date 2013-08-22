172b03855659c37c733f6e0b547b986233f53b9d
rezak18 <savelev.na@mail.ru>
2013-08-23 02:47:21 +0400

    Test2

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello! ;(");
+        System.out.println("Hello!");
+        System.out.println("Hello");
+        System.out.println("Hello");
+
     }
 }
