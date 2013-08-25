7e75e462592a63d9b5d5f3da99807f04a460efbe
rezak18 <savelev.na@mail.ru>
2013-08-25 21:49:17 +0400

    Update

--- src/Human.java
+++ src/Human.java
@@ -1,8 +1,9 @@
 
 public class Human {
 
-    private final int MIN_AGE = 0;
-    private final int MAX_AGE = 130;
+    private static final int MIN_AGE = 0;
+    private static final int MAX_AGE = 130;
+    private static final int MIN_NAME_LENGTH = 2;
 
     private String name;
 
@@ -13,7 +14,7 @@ public class Human {
     }
 
     public void setName (String newName) {
-        if (newName != null && newName.length() >= 2) {
+        if (newName != null && newName.length() >= Human.MIN_NAME_LENGTH) {
             name = newName;
         }
     }
@@ -23,7 +24,7 @@ public class Human {
     }
 
     public void setAge (int newAge) {
-        if (newAge > MIN_AGE && newAge < MAX_AGE)
+        if (newAge > Human.MIN_AGE && newAge < Human.MAX_AGE)
           age = newAge;
         else {
             System.out.println("You are STUPID!");
