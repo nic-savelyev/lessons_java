9b4e2c9f1f538c7b8c18eb33bc077607cdff3631
rezak18 <savelev.na@mail.ru>
2013-09-03 03:10:13 +0400

    Lesson 8 complete!)

--- src/Field.java
+++ src/Field.java
@@ -1,34 +1,57 @@
 
 public class Field {
 
-    private static final int FIELD_SIZE = 10;
-
     private static final char DEFAULT_CEll_VALUE = ' ';
 
-    private char[][] field = new char[FIELD_SIZE][FIELD_SIZE];
+    private static final int DEFAULT_FIELD_SIZE = 3;
+
+    private int fieldSize = 3;
+
+    private final char[][] field;
+
+    public Field() {
+        this(DEFAULT_FIELD_SIZE);
+        System.out.println("In constr1");
+    }
+
+    public Field(int size) {
+        fieldSize = size;
+        field = new char[fieldSize][fieldSize];
+        System.out.println("In constr2");
+    }
+
+ /*   public void setFieldSize (int newFieldSize) {
+        if (newFieldSize > MIN_FIELD_SIZE && newFieldSize < MAX_FiELD_SIZE) {
+            fieldSize = newFieldSize;
+        }
+    }
+   */
+    public int getFieldSize () {
+       return fieldSize;
+    }
 
     public void eraseField() {
-        for (int i = 0; i < FIELD_SIZE; i++) {
+        for (int i = 0; i < fieldSize; i++) {
             eraseLine(i);
         }
     }
 
     public void showField() {
         System.out.println();
-        for (int i = 0; i < FIELD_SIZE; i++) {
+        for (int i = 0; i < fieldSize; i++) {
             showLine(i);
             System.out.println();
         }
     }
 
     private void eraseLine (int lineNumber) {
-        for (int i = 0; i < FIELD_SIZE; i++) {
+        for (int i = 0; i < fieldSize; i++) {
             field [i][lineNumber] = DEFAULT_CEll_VALUE;
         }
     }
 
     private void showLine(int lineNumber) {
-        for (int i = 0; i < FIELD_SIZE; i++) {
+        for (int i = 0; i < fieldSize; i++) {
             showCell(i, lineNumber);
         }
     }
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@ public class Main {
 
     public static void main(String[] args) {
         Field field = new Field();
-        field.eraseField();
+        field.showField();
+
+        field = new Field(12);
         field.showField();
     }
 }
