b4cc113e389cc083fcbfe94fb6a08e4263032cc0
rezak18 <savelev.na@mail.ru>
2013-09-12 01:12:29 +0400

    Revert d5e28ac..5caef00
    This rolls back to commit d5e28ac721c847a52407705793ef240e65b376b9.

--- src/Field.java
+++ src/Field.java
@@ -1,57 +1,34 @@
 
 public class Field {
 
+    private static final int FIELD_SIZE = 10;
+
     private static final char DEFAULT_CEll_VALUE = ' ';
 
-    private static final int DEFAULT_FIELD_SIZE = 3;
-
-    private int fieldSize = 3;
-
-    private final char[][] field;
-
-    public Field() {
-        this(DEFAULT_FIELD_SIZE);
-        System.out.println("In constr1");
-    }
-
-    public Field(int size) {
-        fieldSize = size;
-        field = new char[fieldSize][fieldSize];
-        System.out.println("In constr2");
-    }
-
- /*   public void setFieldSize (int newFieldSize) {
-        if (newFieldSize > MIN_FIELD_SIZE && newFieldSize < MAX_FiELD_SIZE) {
-            fieldSize = newFieldSize;
-        }
-    }
-   */
-    public int getFieldSize () {
-       return fieldSize;
-    }
+    private char[][] field = new char[FIELD_SIZE][FIELD_SIZE];
 
     public void eraseField() {
-        for (int i = 0; i < fieldSize; i++) {
+        for (int i = 0; i < FIELD_SIZE; i++) {
             eraseLine(i);
         }
     }
 
     public void showField() {
         System.out.println();
-        for (int i = 0; i < fieldSize; i++) {
+        for (int i = 0; i < FIELD_SIZE; i++) {
             showLine(i);
             System.out.println();
         }
     }
 
     private void eraseLine (int lineNumber) {
-        for (int i = 0; i < fieldSize; i++) {
+        for (int i = 0; i < FIELD_SIZE; i++) {
             field [i][lineNumber] = DEFAULT_CEll_VALUE;
         }
     }
 
     private void showLine(int lineNumber) {
-        for (int i = 0; i < fieldSize; i++) {
+        for (int i = 0; i < FIELD_SIZE; i++) {
             showCell(i, lineNumber);
         }
     }
--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,7 @@ public class Main {
 
     public static void main(String[] args) {
         Field field = new Field();
-        field.showField();
-
-        field = new Field(12);
+        field.eraseField();
         field.showField();
     }
 }
